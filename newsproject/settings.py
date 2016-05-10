26d9fdd2271d74c92f8d705f125912410f246d59
Mr. James A. Black Jr <mrdesigner@jamesblackjr.com>
2016-05-10 18:03:12 -0500

    Update settings and requirements to support production server

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -114,6 +114,11 @@ elif DJANGO_MODE == 'staging':
             'PORT': os.getenv('DATABASE_PORT', '5432'),
         }
     }
+
+elif DJANGO_MODE == 'production':
+    import dj_database_url
+    # Handles DATABASE_URL environment variable
+    DATABASES = {'default': dj_database_url.config()}
     
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -154,6 +159,7 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'newsproject', 'static'),
