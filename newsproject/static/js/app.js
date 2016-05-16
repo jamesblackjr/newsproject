d21c0fdcf44740d0df2f97d167ce095e8e9f87ad
Mr. James A. Black Jr <mrdesigner@jamesblackjr.com>
2016-05-15 21:39:40 -0500

    Add Scroll to Top button for Medium/Large screens.

--- newsproject/static/js/app.js
+++ newsproject/static/js/app.js
@@ -1,1 +1,19 @@
-// Custom JavaScript for the News Project App
+// Show Back to Top Button when Scrolling Down
+var amountScrolled = 300;
+
+if (window.innerWidth >= 768) {
+	$(window).scroll(function() {
+		if ( $(window).scrollTop() > amountScrolled ) {
+			$('a.back-to-top').fadeIn('slow');
+		} else {
+			$('a.back-to-top').fadeOut('slow');
+		}
+	});
+};
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
